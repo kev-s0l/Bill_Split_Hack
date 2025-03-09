a77fb1fdfd67fa11f06fcde05d76bdfa7ff23a6f
adrixngeles <129955641+adrixngeles@users.noreply.github.com>
2025-03-09 11:58:40 -0400

    sadsadasdasadas
    sad

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 // MongoDB 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
